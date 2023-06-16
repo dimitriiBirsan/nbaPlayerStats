2553b828200edd8cad1dd44cf3841ae0ed85fc1c
Dumitru Birsan <dimitriibirsan@gmail.com>
2023-06-16 10:34:18 +0200

    added games, Stats, teams, PlayerAverage and UserLoginRequest models, added api call to get all stats and save them in the database, added FKs to link Games, Players , Teams and Stats, added new api calls for Login, checkIfUserIsLoggedIn, added support for Cors and Session

---
+++ playerStats/Models/Games.cs
@@ -0,0 +1,13 @@
+﻿namespace playerStats.Models
+{
+    public class Games
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public int Home_team_id { get; set; }
+        public int Home_team_score { get; set; }
+        public int Season { get; set; }
+        public int Visitor_team_id { get; set; }
+        public int Visitor_team_score { get; set;}
+    }
+}
--- playerStats/Services/PlayerService.cs
+++ playerStats/Services/PlayerService.cs
@@ -1,4 +1,6 @@
-﻿using playerStats.Data;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using playerStats.Data;
 using playerStats.Models;
 using System.Diagnostics;
 
@@ -8,10 +10,14 @@ namespace playerStats.Services
     {
         private readonly HttpClient _httpClient;
         private readonly PlayerContext _playerContext;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public PlayerService(HttpClient httpClient)
+
+        public PlayerService(HttpClient httpClient, PlayerContext context, IHttpContextAccessor httpContextAccessor)
         {
             _httpClient = httpClient;
+            _playerContext = context;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<IEnumerable<Player>?> GetAllPlayersAsync()
@@ -60,46 +66,133 @@ namespace playerStats.Services
 
         public async Task<List<Stats>> GetStatsForCurrentSeasonAsync()
         {
-            var defaultLink = "https://www.balldontlie.io/api/v1/season_averages?season=2022";
             var playerStatistics = new List<Stats>();
-            var players = await GetAllPlayersAsync();
+            var currentPage = 1;
+            var totalPages = 0;
 
-            foreach (var item in players)
+            do
             {
+                var response = await _httpClient.GetAsync($"https://www.balldontlie.io/api/v1/stats?seasons[]=2022&per_page=100&page={currentPage}");
 
-                defaultLink = $"https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]={item.Id}";
-                var response = await _httpClient.GetAsync(defaultLink);
-                Debug.WriteLine(defaultLink);
-                if(response.IsSuccessStatusCode)
+                if (response.IsSuccessStatusCode)
                 {
-                    var stats = await response.Content.ReadFromJsonAsync<StatsResponse>();
-                    Console.WriteLine(stats);
-                    if(stats.Data.Count > 0)
-                    {
-                        playerStatistics.AddRange(stats.Data);
+                    var pageData = await response.Content.ReadFromJsonAsync<StatsResponse>();
+                    playerStatistics.AddRange(pageData.Data);
 
+                    totalPages = pageData.Meta.Total_pages;
+                    currentPage = pageData.Meta.Next_page;
+
+                    foreach (var stat in pageData.Data)
+                    {
+                        var existingPlayer = await _playerContext.Players.FindAsync(stat.Player.Id);
+                        var existingStat = await _playerContext.Stats.FindAsync(stat.Id);
+                        var existingTeam = await _playerContext.Teams.FindAsync(stat.Team.Id);
+                        var existingGame = await _playerContext.Games.FindAsync(stat.Game.Id);
+
+                        if (existingPlayer == null)
+                        {
+                            _playerContext.Players.Add(stat.Player);
+                        }
+                        else
+                        {
+                            _playerContext.Players.Attach(existingPlayer);
+                            stat.Player = existingPlayer;
+                        }
+
+                        if (existingTeam == null)
+                        {
+                            _playerContext.Teams.Add(stat.Team);
+                        }
+                        else
+                        {
+                            _playerContext.Teams.Attach(existingTeam);
+                            stat.Team = existingTeam;
+                        }
+
+                        if (existingGame == null)
+                        {
+                            _playerContext.Games.Add(stat.Game);
+                        }
+                        else
+                        {
+                            _playerContext.Games.Attach(existingGame);
+                            stat.Game = existingGame;
+                        }
+                        if(existingStat == null)
+                        {
+                            _playerContext.Stats.Add(stat);
+                        }
+                        await _playerContext.SaveChangesAsync();
                     }
 
+                    // Add a delay of 1 second before fetching the next page
+                    if (currentPage <= totalPages)
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                    }
                 }
-                // Add a delay of 1 second before fetching the next page
-                //if (currentPage <= totalPages)
-                //{
-                await Task.Delay(TimeSpan.FromSeconds(1));
-                //}
+                else
+                {
+                    throw new Exception("Error fetching player data");
+                };
+            } while (currentPage < totalPages);
 
-
-            }
             return playerStatistics;
         }
 
-        public async Task<List<Stats>> GetTrendiestPlayersAsync()
+        public async Task<List<PlayerAverage>> GetTrendiestPlayersAsync()
         {
-            var trendiestPlayers = new List <Stats>();
-            var currentSeasonStats = await GetStatsForCurrentSeasonAsync();
+            var userEmail = _httpContextAccessor.HttpContext.Session.GetString("email");
+            var currentSeason = 2022;
+            var statsForLastGames = 5;
 
-            trendiestPlayers = currentSeasonStats.OrderByDescending(x => x.pts).Take(10).ToList();
+            var favoritePlayerIds = new HashSet<int>();
+            if (userEmail != null)
+            {
+                var user = await _playerContext.Users.Include(u => u.FavoritePlayers).FirstOrDefaultAsync(u => u.Email == userEmail);
+                if (user != null)
+                {
+                    favoritePlayerIds = new HashSet<int>(user.FavoritePlayers.Select(fp => fp.Id));
+                }
+            }
 
-            return trendiestPlayers;
+            var statsRecords = await _playerContext.Stats
+                .Include(s => s.Game)
+                .Include(s => s.Player)
+                .Include (s => s.Team)
+                .Where(s => s.Game.Season == currentSeason)
+                .ToListAsync();
+
+            var groupByPlayer = statsRecords.GroupBy(s => s.PlayerId);
+            var playerAverage = new List<PlayerAverage>();
+
+            foreach (var group in groupByPlayer)
+            {
+                var lastGames = group
+                    .OrderByDescending(s => s.Game.Date)
+                    .Take(statsForLastGames)
+                    .ToList();
+
+                var averagePoints = lastGames.Average(s => s.pts);
+
+                var player = lastGames.Select(s => s.Player).First();
+                var team = lastGames.Select(s => s.Team).First();
+
+                playerAverage.Add(new PlayerAverage
+                {
+                    AveragePoints = averagePoints,
+                    Player = player,
+                    Teams = team,
+                    IsFavorite = favoritePlayerIds.Contains(player.Id)
+                });
+            }
+
+            var topPlayers = playerAverage
+                .OrderByDescending(pa => pa.AveragePoints)
+                .Take(10)
+                .ToList();
+
+            return topPlayers;
         }
     }
 }
