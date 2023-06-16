763cf3b8cd3658ebd6cb71f937028b2ae37a8725
Dumitru Birsan <dimitriibirsan@gmail.com>
2023-06-16 17:48:27 +0200

    fixed session calls for non Cors requests, fixed login and AddFavoritePlayer with email

--- playerStats/Models/User.cs
+++ playerStats/Models/User.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Email { get; set; }
-        public List<Player> FavoritePlayers { get; set; }
+        public List<Player> FavoritePlayers { get; set; } = new List<Player>();
 
 
     }
--- playerStats/Program.cs
+++ playerStats/Program.cs
@@ -4,9 +4,10 @@ using playerStats.Services;
 using playerStats.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var MyAllowSpecificOrigins = "frontEndPolicy";
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -25,13 +26,14 @@ builder.Services.AddCors(options =>
 		policy =>
 		{
 			policy.WithOrigins("http://localhost:3000")
-					.AllowAnyMethod()
-					.AllowAnyHeader();
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
         });
 });
 builder.Services.AddSession(options => {
 	options.IdleTimeout = TimeSpan.FromMinutes(31);
-	options.Cookie.HttpOnly = true;
+	options.Cookie.HttpOnly = false;
 	options.Cookie.IsEssential = true;
 });
 
@@ -47,22 +49,34 @@ if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 //app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseDefaultFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "./web_interface/build")),
+    RequestPath = ""
+});
 app.UseCors(MyAllowSpecificOrigins);
 
-app.MapGet("/api/GetAllPlayersAsync", async ( PlayerService playerService) =>
+app.MapGet("/api/GetAllStatsForCurrentSeasonAsync", async ( PlayerService playerService) =>
 {
-    var players = await playerService.GetAllPlayersAsync();
+    var players = await playerService.GetStatsForCurrentSeasonAsync();
     return players;
 
 }).WithOpenApi();
 
-app.MapGet("/api/checkIfUserIsLoggedIn", async (UserService userService) =>
+app.MapGet("/api/GetAndSaveStatsAsync", async (PlayerService playerService) =>
 {
-	return await userService.CheckIfUserIsLoggedIn();
+	return await playerService.GetAndSaveStatsAsync();
+}).WithOpenApi();
+
+app.MapGet("/api/checkIfUserIsLoggedIn", async (UserService userService, HttpContext httpContext) =>
+{
+	return await userService.CheckIfUserIsLoggedIn(httpContext);
 }).WithOpenApi();
 
 app.MapPost("/api/CreateUser", async (UserService userService, string email) =>
@@ -71,22 +85,23 @@ app.MapPost("/api/CreateUser", async (UserService userService, string email) =>
 	return user;
 }).WithOpenApi();
 
-app.MapPost("/api/AddFavoritePlayer", async (UserService userService, Player player) =>
+app.MapPost("/api/AddFavoritePlayer", async ([FromBody] AddFavoritePlayer favoritePlayer,  UserService userService,  HttpContext httpContext) =>
 {
-	return await userService.AddFavoritePlayerAsync( player);
+	return await userService.AddFavoritePlayerAsync(favoritePlayer, httpContext);
 }).WithOpenApi();
 
 
-app.MapGet("/api/GetTrendiestPlayer", async (PlayerService playerService) =>
+app.MapGet("/api/GetTrendiestPlayer", async ([FromBody] string? userEmail, PlayerService playerService, HttpContext httpContext) =>
 {
-	var trendiestPlayers = await playerService.GetTrendiestPlayersAsync();
+	var trendiestPlayers = await playerService.GetTrendiestPlayersAsync(userEmail, httpContext);
     return trendiestPlayers;
 }).WithOpenApi();
 
-app.MapPost("/api/login", async ( UserLoginRequest loginRequest, UserService userService) =>
+app.MapPost("/api/login", async ( UserLoginRequest loginRequest, UserService userService, HttpContext httpContext) =>
 {
-	return await userService.LoginAsync(loginRequest.Email);
+	return await userService.LoginAsync(loginRequest.Email, httpContext);
 }).WithOpenApi();
+
 
 
 using (var scope = app.Services.CreateScope())
